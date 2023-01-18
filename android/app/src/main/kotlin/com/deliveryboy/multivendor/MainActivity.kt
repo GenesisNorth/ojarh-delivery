4c3da4464b2a8498370980c860e7b16ab48eadb9
Genesis <habilagenesis@gmail.com>
2023-01-18 09:16:35 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/deliveryboy/multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.retailshopping.ojarh.deliveryboy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Helper/constant.dart
@@ -0,0 +1,21 @@
+// your application name 
+final String appName = 'Ojarh Delivery boy';
+
+//Set country code
+String defaultCountryCode = 'NIG';
+
+// issue name (Note : Please do not change this value)
+const String issuerName = 'eshop';
+
+//SECRET KEYS FOR CONNECT YOUR APP WITH ADMIN PANEL 
+final String baseUrl =
+    'https://retailshopping.ojarh.com/delivery_boy/app/v1/api/';
+final String jwtKey = "24592c34217252e30fef6201b90c89532901c942";
+
+final int timeOut = 50;
+const int perPage = 10;
+String? supportedLocale;
+String? IS_APP_UNDER_MAINTENANCE;
+String? MAINTENANCE_MESSAGE;
+
+
